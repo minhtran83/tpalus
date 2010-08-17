fc48816d9631082d102ae14641e587a050388960
racezhang <racezhang@454cb45b-5f8a-43e4-73d6-c6a0c25fc75f>
2010-08-17 13:50:09 +0000

    evaluation starts

--- trunk/ tpalus --username racezhang/Palus/apachecollectionexperiment/generatedtest/ApacheCollectionJUnitTest.java
+++ trunk/ tpalus --username racezhang/Palus/apachecollectionexperiment/generatedtest/ApacheCollectionJUnitTest.java
@@ -2,7 +2,7 @@ package generatedtest;
 import junit.framework.*;
 import junit.textui.*;
 
-public class TinySQLUnitTest extends TestCase {
+public class ApacheCollectionJUnitTest extends TestCase {
 
   public static void main(String[] args) {
     TestRunner runner = new TestRunner();
@@ -12,16 +12,14 @@ public class TinySQLUnitTest extends TestCase {
     }
   }
 
-  public TinySQLUnitTest(String name) {
+  public ApacheCollectionJUnitTest(String name) {
     super(name);
   }
 
   public static Test suite() {
     TestSuite result = new TestSuite();
-    result.addTest(new TestSuite(TinySQLUnitTest0.class));
-    result.addTest(new TestSuite(TinySQLUnitTest1.class));
-    result.addTest(new TestSuite(TinySQLUnitTest2.class));
-    result.addTest(new TestSuite(TinySQLUnitTest3.class));
+    result.addTest(new TestSuite(ApacheCollectionJUnitTest0.class));
+    result.addTest(new TestSuite(ApacheCollectionJUnitTest1.class));
     return result;
   }
 
