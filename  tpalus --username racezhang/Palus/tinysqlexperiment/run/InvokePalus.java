9c4a0dae124f40d30bef966f9867a57cf93a587c
racezhang@gmail.com <racezhang@gmail.com@454cb45b-5f8a-43e4-73d6-c6a0c25fc75f>
2011-05-02 05:30:11 +0000

    add harness to invoke palus for tinysql

---
+++  tpalus --username racezhang/Palus/tinysqlexperiment/run/InvokePalus.java
@@ -0,0 +1,16 @@
+package run;
+
+import java.io.IOException;
+
+/**
+ * @author szhang@cs.washington.edu (Sai Zhang)
+ */
+public class InvokePalus {
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		 args = new String[]{"--time_limit=50", "--class_file=./tinysqlexperiment/tinysqlclass.txt"
+				  , "--trace_file=./tinysql-10stmts_trace.model"};
+		  palus.main.OfflineMain.main(args);
+	}
+	
+}
